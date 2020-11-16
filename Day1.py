a6e2f308bf2750251827cd0eeeb0023e8203d788
vishalsharma499 <70803219+vishalsharma499@users.noreply.github.com>
2020-11-16 20:02:57 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,25 @@
+# Answer 1
+print("Answer 1")
+print("RAINBOW")
+print("-----------------------")
+# Answer 2
+print("Answer 2")
+print("LETS UPGRADE")
+print("-----------------------")
+# Answer 3
+print("Answer 3")
+costPrice=input()
+sellingPrice=input()
+if costPrice>sellingPrice:
+    print("Loss")
+elif costPrice<sellingPrice:
+    print("Profit")
+else:
+    print("Neither")
+print("-----------------------")
+# Answer 4
+print("Answer 4")
+euro=int(input())
+rupees=euro*80
+print(rupees)
+print("-----------------------")
